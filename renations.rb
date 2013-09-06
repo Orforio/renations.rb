a64565e2a3177a2e0ec12b1a4add8f29c42626cd
Richard Whittaker <richard.whittaker@bbc.co.uk>
2013-09-06 15:24:21 +0100

    Adds command-line argument support

--- renations.rb
+++ renations.rb
@@ -1,4 +1,23 @@
-def rename_files(directory_source, directory_destination, directory_fileextension = "png")
+def check_arguments
+	directory_source = ARGV[0]
+	directory_destination = ARGV[1]
+	directory_fileextension = ARGV[2] ||= "png"
+
+	unless directory_source && directory_destination
+		puts "Usage: $> ruby renations.rb source_folder destination_folder file_extension"
+		puts "Example: $> ruby renations.rb images/welsh/ images/english/ jpg"
+		exit
+	end
+
+	puts "Source directory: #{directory_source}"
+	puts "Destination directory: #{directory_destination}"
+	puts "File extension: #{directory_fileextension}"
+	puts "Is this correct? Type y to continue."
+
+	rename_files(directory_source, directory_destination, directory_fileextension) if STDIN.gets.chomp == "y"
+end
+
+def rename_files(directory_source, directory_destination, directory_fileextension)
 	source = Dir.new(directory_source) if File.directory?(directory_source)
 	destination = Dir.new(directory_destination) if File.directory?(directory_destination)
 	destination_ids = Array.new
@@ -34,11 +53,4 @@ def rename_files(directory_source, directory_destination, directory_fileextensio
 	puts "Renamed: #{files_changed}, Skipped: #{files_skipped}"
 end
 
-# Hardcoded values for testing
-# TODO: Ask user for input
-
-directory_source = "D:/Richard/Developer/renations.rb/naiseanta5_stills/"
-directory_destination = "D:/Richard/Developer/renations.rb/photographs_cropped/"
-directory_fileextension = "jpg"
-
-rename_files(directory_source, directory_destination, directory_fileextension)
+check_arguments

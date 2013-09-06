92dde130296d75de52711d82ba09bfccce735d94
Richard Whittaker <richard.whittaker@bbc.co.uk>
2013-09-06 11:37:17 +0100

    Adds actual renaming and additional feedback

--- renations.rb
+++ renations.rb
@@ -2,6 +2,7 @@ def rename_files(directory_source, directory_destination, directory_fileextensio
 	source = Dir.new(directory_source) if File.directory?(directory_source)
 	destination = Dir.new(directory_destination) if File.directory?(directory_destination)
 	destination_ids = Array.new
+	files_changed, files_skipped = 0, 0
 
 	unless source && destination
 		puts "Could not find directories given"
@@ -16,17 +17,21 @@ def rename_files(directory_source, directory_destination, directory_fileextensio
 
 	destination_list.each do |destination_filename|
 		destination_ids.push(destination_filename[/\/(p?\d+)_/, 1])
-		puts destination_filename[/\/(p?\d+)_/, 1]
 	end
 
 	source_list.each do |source_filename|
-		puts source_filename
-		if destination_ids.index(source_filename[/\/(p?\d+)_/, 1])
-			puts "Yes, " + source_filename[/\/(p?\d+)_/, 1] + " is present."
+		if destination_index = destination_ids.index(source_filename[/\/(p?\d+)_/, 1])
+			#puts "#{destination_list[destination_index]} => #{directory_destination + File.basename(source_filename)}"
+			File.rename(destination_list[destination_index], directory_destination + File.basename(source_filename))
+			files_changed += 1
 		else
-			puts "No, " + source_filename[/\/(p?\d+)_/, 1] + " is not present."
+			puts "Skipping #{source_filename}..."
+			files_skipped += 1
 		end
 	end
+
+	puts "Renaming finished."
+	puts "Renamed: #{files_changed}, Skipped: #{files_skipped}"
 end
 
 # Hardcoded values for testing

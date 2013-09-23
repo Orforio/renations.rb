5b4240ecebb76638c20320e6c5d2935a4ce4eb43
Richard Whittaker <richard.whittaker@bbc.co.uk>
2013-09-23 15:27:47 +0100

    Handle both graphics and photos tabs

--- renations.rb
+++ renations.rb
@@ -5,14 +5,13 @@ def check_arguments
 	directory_source = ARGV[0]
 	directory_destination = ARGV[1]
 	directory_fileext = ARGV[2] ||= "png"
-	spreadsheet_sheet = ARGV[3] ||= 1
+	spread_sheet = ARGV[3] ||= 0
 	use_spreadsheet = false
 
-	directory_fileextension = directory_fileext.delete(".")
-
 	unless directory_source && directory_destination
-		puts "Usage: $ ruby renations.rb source_folder|migration_log.xlsx destination_folder file_extension"
-		puts "Example: $ ruby renations.rb images/welsh/ images/english/ jpg"
+		puts "Usage: $ ruby renations.rb source_folder|migration_log.xlsx destination_folder (file_extension) (graphics|photos)"
+		puts "Directory Example: $ ruby renations.rb images/welsh/ images/english/ jpg"
+		puts "Spreadsheet Example: $ ruby renations.rb images/welsh/migration_log.xlsx images/english/ jpg photos"
 		exit
 	end
 
@@ -30,8 +29,12 @@ def check_arguments
 			exit
 		end
 	end
+
+	directory_fileextension = directory_fileext.delete(".")
 	
 	directory_destination += "/" unless directory_destination[/\/\z/]
+
+	spread_sheet == "photos" ? spreadsheet_sheet = 1 : spreadsheet_sheet = 0
 
 	unless File.directory?(directory_destination)
 		puts "Destination not found."
@@ -41,12 +44,13 @@ def check_arguments
 	puts use_spreadsheet ? "Source migration log: #{directory_source}" : "Source directory: #{directory_source}"
 	puts "Destination directory: #{directory_destination}"
 	puts "File extension: #{directory_fileextension}"
+	puts "Sheet: #{spreadsheet_sheet}" if use_spreadsheet
 	puts "Is this correct? Type y to continue."
 
 	STDIN.gets.chomp == "y" ? use_spreadsheet ? rename_spreadsheet(directory_source, directory_destination, directory_fileextension, spreadsheet_sheet) : rename_files(directory_source, directory_destination, directory_fileextension) : exit
 end
 
-def rename_spreadsheet(spreadsheet_source, directory_destination, directory_fileextension, spreadsheet_sheet = 1)
+def rename_spreadsheet(spreadsheet_source, directory_destination, directory_fileextension, spreadsheet_sheet)
 	require 'roo'
 
 	source_list, destination_ids = Array.new, Array.new
@@ -55,39 +59,62 @@ def rename_spreadsheet(spreadsheet_source, directory_destination, directory_file
 	puts "Please wait..."
 
 	sheet = Roo::Excelx.new(spreadsheet_source)
-	sheet.sheet(0) # Currently hardcoded for testing purposes
+	sheet.sheet(spreadsheet_sheet)
 
-	sheet.each(:job => 'Job No.', :filename => '^New\sfilename\s', :wlarge => '^large\swidth', :wmedium => '^medium\swidth', :wsmall => '^small\swidth') do |hash|
-		if hash[:job] && hash[:job][/^(p?\d+)/, 1]
-			if hash[:wlarge] && hash[:wmedium] && hash[:wsmall]
-				puts "WARNING: #{hash[:job]} has an invalid LARGE width." unless hash[:wlarge] == IMAGE_SIZES[0] || hash[:wlarge] == IMAGE_SIZES[2]
-				puts "WARNING: #{hash[:job]} has an invalid MEDIUM width." unless hash[:wmedium] == IMAGE_SIZES[1] || hash[:wmedium] == IMAGE_SIZES[2]
-				puts "WARNING: #{hash[:job]} has an invalid SMALL width." unless hash[:wsmall] == IMAGE_SIZES[2]
-				hash[:wlarge], hash[:wmedium], hash[:wsmall] = hash[:wlarge].to_i, hash[:wmedium].to_i, hash[:wsmall].to_i
+	if spreadsheet_sheet == 1
+		sheet.each(:job => 'Job No.', :filename => '^New\sfilename\s') do |hash|
+			if hash[:job] && hash[:job][/^(p?\d+)/, 1]
+				source_list << hash
 			end
-			source_list << hash
+		end
+	else
+		sheet.each(:job => 'Job No.', :filename => '^New\sfilename\s', :wlarge => '^large\swidth', :wmedium => '^medium\swidth', :wsmall => '^small\swidth') do |hash|
+			if hash[:job] && hash[:job][/^(p?\d+)/, 1]
+				if hash[:wlarge] && hash[:wmedium] && hash[:wsmall]
+					puts "WARNING: #{hash[:job]} has an invalid LARGE width." unless hash[:wlarge] == IMAGE_SIZES[0] || hash[:wlarge] == IMAGE_SIZES[2]
+					puts "WARNING: #{hash[:job]} has an invalid MEDIUM width." unless hash[:wmedium] == IMAGE_SIZES[1] || hash[:wmedium] == IMAGE_SIZES[2]
+					puts "WARNING: #{hash[:job]} has an invalid SMALL width." unless hash[:wsmall] == IMAGE_SIZES[2]
+					hash[:wlarge], hash[:wmedium], hash[:wsmall] = hash[:wlarge].to_i, hash[:wmedium].to_i, hash[:wsmall].to_i
+				end
+				source_list << hash
+			end
 		end
 	end
 
 	destination_list = Dir.glob(directory_destination + "*." + directory_fileextension)
 
 	destination_list.each do |destination_filename|
-		destination_ids << [:job => destination_filename[/\/(p?\d+)_/, 1], :size => destination_filename[/_([0-9]{3})\.#{directory_fileextension}$/, 1].to_i]
+		if spreadsheet_sheet == 1
+			destination_ids << destination_filename[/\/(p?\d+)_/, 1]
+		else
+			destination_ids << [:job => destination_filename[/\/(p?\d+)_/, 1], :size => destination_filename[/_([0-9]{3})\.#{directory_fileextension}$/, 1].to_i]
+		end
 	end
 
 	source_list.each do |source_hash|
-		image_widths = [source_hash[:wlarge], source_hash[:wmedium], source_hash[:wsmall]].uniq
-
-		image_widths.each do |image_width|
-			if destination_index = destination_ids.index do |destination_hash, x|
-					(destination_hash[:job] == source_hash[:job]) && (destination_hash[:size] == image_width)
-				end
-				#puts "Renaming #{destination_list[destination_index]} to #{directory_destination + source_hash[:filename].to_s + "_" + image_width.to_s + "." + directory_fileextension}"
-				File.rename(destination_list[destination_index], directory_destination + source_hash[:filename].to_s + "_" + image_width.to_s + "." + directory_fileextension)
+		if spreadsheet_sheet == 1
+			if destination_index = destination_ids.index(source_hash[:job])
+				#puts "Renaming #{destination_list[destination_index]} to #{directory_destination + source_hash[:filename].to_s + "." + directory_fileextension}"
+				File.rename(destination_list[destination_index], directory_destination + source_hash[:filename].to_s + "." + directory_fileextension)
 				files_changed += 1
 			else
-				puts "Skipping #{source_hash[:filename]}_#{image_width}"
+				puts "Skipping #{source_hash[:filename]}"
 				files_skipped += 1
+			end
+		else
+			image_widths = [source_hash[:wlarge], source_hash[:wmedium], source_hash[:wsmall]].uniq
+
+			image_widths.each do |image_width|
+				if destination_index = destination_ids.index do |destination_hash, x|
+						(destination_hash[:job] == source_hash[:job]) && (destination_hash[:size] == image_width)
+					end
+					#puts "Renaming #{destination_list[destination_index]} to #{directory_destination + source_hash[:filename].to_s + "_" + image_width.to_s + "." + directory_fileextension}"
+					File.rename(destination_list[destination_index], directory_destination + source_hash[:filename].to_s + "_" + image_width.to_s + "." + directory_fileextension)
+					files_changed += 1
+				else
+					puts "Skipping #{source_hash[:filename]}_#{image_width}"
+					files_skipped += 1
+				end
 			end
 		end
 	end
